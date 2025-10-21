85b37c68a5bae859955c6f0963d70ad88213485d
Tsedeke Techane <tsedeketechane@gmail.com>
2025-10-21 22:18:14 +0300

    Setup React project with router and vanilla CSS

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage"
+import CheckoutPage from "./pages/CheckoutPage";
+import { OrdersPage } from "./pages/OrdersPage";
+import { TrackingPage } from "./pages/TrackingPage";
+
+function App() {
+
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/tracking" element={<TrackingPage />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
